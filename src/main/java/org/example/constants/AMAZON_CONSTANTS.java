b3ee42f1d65713b96db66af4e493ea655335a6fd
anil_desai <anil.hhh.desai@gmail.com>
2022-02-03 22:23:49 +1100

    Removed un-necessary files

--- src/main/java/org/example/constants/AMAZON_CONSTANTS.java
+++ src/main/java/org/example/constants/AMAZON_CONSTANTS.java
@@ -6,10 +6,10 @@ import java.net.URI;
 
 public final class AMAZON_CONSTANTS {
 
-    public static final Regions CLIENT_REGION = com.amazonaws.regions.Regions.AP_SOUTHEAST_2;
-    public static final String SECRET_KEY = "9YClKg2GVCgIGP82dNwOJldbq3hcgC0qRhbtqZal";
-    public static final String ACCESS_KEY = "AKIAZUO64Q7BB5CLW7MT";
-    public static final String BUCKET_NAME = "candidate-108-s3-bucket";
+    public static final Regions CLIENT_REGION = null;
+    public static final String SECRET_KEY = "@Sec key";
+    public static final String ACCESS_KEY = "@ACC Key";
+    public static final String BUCKET_NAME = "@Buck Name";
     public static final String ELLIPSIS = "ellipsis";
     public static final String PARQUET_SCHEMA_FILE_EXTENSION = ".avsc";
     public static final String PARQUET_FILE_EXTENSION = ".parquet";
